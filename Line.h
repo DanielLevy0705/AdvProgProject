680277b3bf6154aa914fb8f46b176b70d8b61046
Fatuity <nxhvnkl123@gmail.com>
2018-12-18 07:59:47 +0200

    Line popFirst is long runtime (O(n))

---
+++ Line.h
@@ -0,0 +1,46 @@
+//
+// Created by fatuity on 12/18/18.
+//
+
+#ifndef PROJ1_LINE_H
+#define PROJ1_LINE_H
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Line {
+    vector <string> line;
+public:
+    Line(vector <string> newLine) {
+        line = newLine;
+    }
+
+    string popFirst() {
+        string retVal = line.at(0);
+        line = vector<string>(&line[1], &line[size - 1]);
+
+    }
+
+    string operator[](size_t n) {
+        return line.at(n);
+    }
+
+    int size() {
+        return line.size();
+    }
+
+    bool has(int n) {
+        return (!line.empty());
+    }
+
+    void addWord(string newWord) {
+        line.push_back(newWord);
+    }
+
+};
+
+
+#endif //PROJ1_LINE_H
