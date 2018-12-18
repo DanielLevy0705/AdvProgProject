cdb0c4d0e64b30a4d2b5c594e7856d2be1941616
Fatuity <nxhvnkl123@gmail.com>
2018-12-18 23:13:47 +0200

    lexer fix

--- Lexer.h
+++ Lexer.h
@@ -11,29 +11,35 @@ class Lexer {
         int idx = 0;
         string newWord, substring;
         bool newWordAddition = false;
-        while (idx == input.size()) {
-
+        //run for every char of the line.
+        while (idx != input.size()) {
+            //check if the char is space, if it is, add the last word and nullify the next word.
             if (isspace(input[idx])) {
                 if (newWordAddition) {
                     line.addWord(newWord);
                     newWordAddition = false;
                     newWord = "";
                 }
-            } else if (!isalpha(input[idx]) && !isdigit(input[idx])) {
+                //if its not alphabet,number or a dot, its an operator.
+            } else if (!isalpha(input[idx]) && !isdigit(input[idx]) && (input[idx] != '.')) {
                 if (newWordAddition) {
+                    //add new word and nullify the next.
                     line.addWord(newWord);
                     newWordAddition = false;
                     newWord = "";
                 }
+                //add the operator then nullify the next word.
                 newWord += input[idx];
                 line.addWord(newWord);
                 newWord = "";
+                //if its a digit or a letter add it to the word, and notify that youre buliding a word.
             } else {
                 newWord += input[idx];
                 newWordAddition = true;
             }
             idx++;
         }
+        return line;
     }
 };
 
