c404d13f6bf6657dc59a83ab848dd6adf7fb76fe
elron <elronbandel@gmail.com>
2018-12-22 22:54:26 +0200

    inputer first version

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 int main() {
     BindedSymbolMap* symap = new BindedSymbolMap; //will be connected to data server and flight server
     Expressioner* expressioner = new Expressioner(map<string, Expression> =
-                                                  new {"var",VarCommand(&expressioner, symap)},
-                                                  new {"openDataServer",DataServerOpenCommand(&expressioner, dataServer)},
-                                                  new {"connect",ConnectCommend(&expressioner, table)});
+                                                  new {"var",VarCommand(expressioner, symap)},
+                                                  new {"openDataServer",DataServerOpenCommand(expressioner, symap)},
+                                                  new {"connect",ConnectCommend(expressioner, symap)});
 
     while (expressioner.on()) {
         expressioner.popNext().calculate();
