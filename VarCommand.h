8dce9053ae487a0ae75ca4c662150ee112e922c3
elron <elronbandel@gmail.com>
2018-12-23 11:29:41 +0200

    some changes

---
+++ VarCommand.h
@@ -0,0 +1,15 @@
+//
+// Created by elronbandel on 12/23/18.
+//
+
+#include "Expression.h"
+
+#ifndef VARCOMMAND_H
+
+class VarCommand: public Expression {
+
+};
+
+#define VARCOMMAND_H
+
+#endif //UNTITLED4_VARCOMMAND_H
